99bc52d1290871f2b975ac31ec83a462ced23363
unknown <mnlikhin@gmail.com>
2025-04-03 13:23:38 +0530

    calculator

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,2 +1,27 @@
-import { calculate } from "./index";
+import {calculate} from './data/data.js'
 const topicGrid=document.querySelector('.topic-grid');
+let sidebarHTML='';
+calculate.forEach((topic)=>{
+  sidebarHTML+=`<div class="topics-container" data-title="${topic.title}">
+            <div class="image">
+              <img src="./image/chevron-down-svgrepo-com.svg" class="icon" />
+            </div>
+            <div class="topic-name">${topic.title}</div>
+           
+          </div>
+           <div class="topic-operations">
+            <ul class="operationss">
+              ${topic.operations.map((op)=>`<li>${op}</li>`).join("")}
+            </ul>
+            </div>
+          `
+})
+topicGrid.innerHTML=sidebarHTML;
+const topicConatiner=document.querySelectorAll('.topics-container');
+const topicOperation=document.querySelectorAll('.topic-operations ul');
+topicConatiner.forEach((op)=>{
+  op.nextElementSibling.firstElementChild.classList.remove('active');
+  op.addEventListener('click',()=>{
+   op.nextElementSibling.firstElementChild.classList.add('active');
+  })
+});
